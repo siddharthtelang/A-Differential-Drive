06764979ceb4d91912efa4e3ee7d4445cc95584d
Siddharth Telang <stelang@umd.edu>
2021-04-18 23:54:06 -0400

    Add conditions for check for obstacle space

--- Obstacle.py
+++ Obstacle.py
@@ -29,3 +29,48 @@ class Obstacle:
         self.rect2_length = 1.5
         self.rect2_width = 2
 
+    def isInObstacleSpace(self, x, y):
+        # circle1 obstacle
+        x_offset = self.circle1_x_offset
+        y_offset = self.circle1_y_offset
+        radius = self.circle1_radius
+        if ((x-x_offset)**2 + (y-y_offset)**2 <= radius):
+          print('Inside circle 1; avoid')
+          return 1
+
+        # circle2 obstacle
+        x_offset = self.circle2_x_offset
+        y_offset = self.circle2_y_offset
+        radius = self.circle2_radius + self.clearance
+        if ((x-x_offset)**2 + (y-y_offset)**2 <= radius):
+          print('Inside circle 2; avoid')
+          return 1
+
+        # square obstacle
+        x1 = self.square1_corner1_x - self.clearance
+        x2 = x1 + self.square_side + 2*self.clearance
+        y1 = self.square1_corner1_y - self.clearance
+        y2 = y1 + self.square_side  + 2*self.clearance
+        if (x >= x1 and x <= x2 and y >= y1 and y <= y2):
+            print('Inside square, avoid')
+            return 1
+
+        #rectangle obstacle 1
+        x1 = self.rect1_corner1_x - self.clearance
+        x2 = x1 + self.rect1_length + 2*self.clearance
+        y1 = self.rect1_corner1_y - self.clearance
+        y2 = y1 + self.rect1_width  + 2*self.clearance
+        if (x >= x1 and x <= x2 and y >= y1 and y <= y2):
+            print('Inside rectangle 1, avoid')
+            return 1
+
+        #rectangle obstacle 2
+        x1 = self.rect2_corner1_x - self.clearance
+        x2 = x1 + self.rect2_length + 2*self.clearance
+        y1 = self.rect2_corner1_y - self.clearance
+        y2 = y1 + self.rect2_width  + 2*self.clearance
+        if (x >= x1 and x <= x2 and y >= y1 and y <= y2):
+            print('Inside rectangle 1, avoid')
+            return 1
+
+        return 0
