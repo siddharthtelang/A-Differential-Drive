5b5228995d7bd6fc79137fad3155fee454fbfa2a
sakshikakde <kakdesakshi@gmail.com>
2021-04-18 23:45:28 -0400

    obstacle visualization

--- Obstacle.py
+++ Obstacle.py
@@ -1,10 +1,31 @@
+
+
 class Obstacle:
     def __init__(self, clearance):
         self.x = 10
         self.y = 10
-        self.clearance = 0
-        self.robot_radius = 0.4 + self.clearance
-        self.circle_radius = 1
-        self.circle_x_offset = 2
-        self.circle_y_offset_1 = 2
-        self.circle_y_offset_2 = 8
+        self.clearance = clearance
+        self.robot_radius = 0.354 / 2
+        self.clearance = self.robot_radius + self.clearance
+
+        self.circle1_radius = 1
+        self.circle2_radius = 1
+        self.circle1_x_offset = 2
+        self.circle1_y_offset = 2
+        self.circle2_x_offset = 2
+        self.circle2_y_offset = 8
+
+        self.square1_corner1_x = 0.25
+        self.square1_corner1_y = 4.25
+        self.square_side = 1.5
+
+        self.rect1_corner1_x = 3.75
+        self.rect1_corner1_y = 4.25
+        self.rect1_length = 2.5
+        self.rect1_width = 1.5
+
+        self.rect2_corner1_x = 7.25
+        self.rect2_corner1_y = 2
+        self.rect2_length = 1.5
+        self.rect2_width = 2
+
